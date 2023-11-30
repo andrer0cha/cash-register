2ed90deff61dbbd1b202d79447d06d28c00c70e2
André Rocha <andre.cgrocha@gmail.com>
2023-11-30 13:00:03 -0300

    [feature] Add association User<>Cart

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+  has_one :cart
+
   validates :email, uniqueness: true, presence: true
   validates_presence_of(:first_name, :last_name)
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,8 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:first_name) }
     it { is_expected.to validate_presence_of(:last_name) }
   end
+
+  describe 'associations' do
+    it { is_expected.to have_one(:cart) }
+  end
 end
