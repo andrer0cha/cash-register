56fe5e0cdd27a8ad502b3c07769ef8c5205339ad
André Rocha <andre.cgrocha@gmail.com>
2023-11-30 11:39:00 -0300

    [feature] Add User model with specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_29_193331) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_30_135507) do
   create_table "products", force: :cascade do |t|
     t.string "code", null: false
     t.string "name", null: false
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_29_193331) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["code"], name: "index_products_on_code", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    subject do
+      create(:user)
+    end
+
+    it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+  end
+end
