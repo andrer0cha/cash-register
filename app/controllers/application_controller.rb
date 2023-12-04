c1cf80339da0827072bac3c41aa24ed9e6546572
André Rocha <andre.cgrocha@gmail.com>
2023-12-04 13:06:00 -0300

    [refactor] Add helpful methods to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,26 @@
 # frozen_string_literal: true
 
 class ApplicationController < Sinatra::Base
-  configure do
-    enable :logging
-    use Rack::CommonLogger
+  set :default_content_type, :json
+
+  after do
+    response.body = response.body.to_json unless response.body.is_a? String
+  end
+
+  def current_user
+    User.find_by(
+      first_name: 'Current',
+      last_name: 'User',
+      email: 'current.user@example.com'
+    )
+  end
+
+  private
+
+  def format_response(body:, status:)
+    response.body = body
+    response.status = status
+
+    response
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class ProductsController < ApplicationController
-  after do
-    response.body = response.body.to_json unless response.body.is_a? String
-  end
-
-  get '/products' do
-    Product.all
+  get '/' do # GET /products/
+    format_response(
+      body: Product.all,
+      status: 200
+    )
   end
 end
